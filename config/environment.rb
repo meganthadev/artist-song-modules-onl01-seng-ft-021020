d9d99cca59ca47626d3142b3f34eb632c71ff740
Megan Horsfield <meganthadev@gmail.com>
2020-03-03 02:59:02 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'pry'
 require_relative '../lib/concerns.rb/findable'
 require_relative '../lib/concerns.rb/memorable'
 require_relative '../lib/concerns.rb/paramable'
+require_relative '../lib/concerns.rb/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
